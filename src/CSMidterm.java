0340658a46e7796a9254ca7b0e785e9c52feaa0b
Yumibyte <ashraigosa@gmail.com>
2020-10-10 22:24:45 -0700

    working between classes and offense/defense levels

--- src/CSMidterm.java
+++ src/CSMidterm.java
@@ -1,36 +1,42 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CSMidterm {
 
     public static class Foods {
         public static ArrayList<Foods> kirbyFoods = new ArrayList<Foods>();
+
         private String foodName;
         private double healAmount;
 
         protected void createFood(String foodName, double healAmount) {
-            this.foodName = foodName;
-            this.healAmount = healAmount;
-            kirbyFoods.add(this);
+            Foods foods = new Foods();
+            foods.foodName = foodName;
+            foods.healAmount = healAmount;
+            kirbyFoods.add(foods);
         }
 
         protected ArrayList<Foods> readFoods() {
-            return kirbyFoods;
+            return this.kirbyFoods;
         }
     }
 
     public static class Skills extends Foods {
         public static ArrayList<Skills> kirbySkills = new ArrayList<Skills>();
+
         private String skillDescription;
         private double skillDamage;
 
         protected void createSkill(String skillDescription, double skillDamage) {
-            this.skillDescription = skillDescription;
-            this.skillDamage = skillDamage;
-            kirbySkills.add(this);
+            Skills skills = new Skills();
+            skills.skillDescription = skillDescription;
+            skills.skillDamage = skillDamage;
+            kirbySkills.add(skills);
         }
+
         protected ArrayList<Skills> readSkills() {
-            return kirbySkills;
+            return this.kirbySkills;
         }
     }
 
@@ -38,36 +44,55 @@ public class CSMidterm {
 
         private String username;
         private double health;
+        private String offense;
+        private String defense;
 
         Kirby(String username) {
             this.health = 100;      // Predefined health
             this.username = username;
-//            kirbyCharacters.add(this);
         }
 
         public void readStats() {
+            ArrayList<Skills> skillsList = this.readSkills();
+            ArrayList<Foods> foodsList = this.readFoods();
+
+            // Evaluate Kirby's total defense and offense based on skills/healing items equipped
+            int totalDamage = 0;
+            int totalHeal = 0;
+
+            for (int i = 0; i < skillsList.size(); i ++) {
+                totalDamage += skillsList.get(i).skillDamage;
+            }
+            for (int j = 0; j < foodsList.size(); j ++) {
+                totalHeal += foodsList.get(j).healAmount;
+            }
+
+            if (totalDamage < 50) {
+                this.offense = "weak";
+            }
+            else if (totalDamage < 200) {
+                this.offense = "novice";
+            } else {
+                this.offense = "all powerful boss";
+            }
+
+            if (totalHeal < 50) {
+                this.defense = "weak";
+            }
+            else if (totalHeal < 200) {
+                this.offense = "novice";
+            } else {
+                this.offense = "all powerful boss";
+            }
 
             // setup to read KirbyCharacters
             System.out.println("CurrentStats\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
             System.out.println("username: " + this.username);
             System.out.println("health: " + this.health);
-            System.out.println("Kirby has " + this.readSkills().size() + " powerful skill(s)");
-            System.out.println("Kirby has " + this.readFoods().size() + " yummy food(s) equipped\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
-//            System.out.println("Kirby's level is " + kirbyLevel);
-            /**
-            for (int i = 0; i < Skills.kirbySkills.size(); i ++) {
-                // TODO: change to read from current Kirby character
-//                System.out.println("Skill description: " + Skills.kirbySkills.get(i).skillDescription);
-//                System.out.println("Skill damage: " + Skills.kirbySkills.get(i).skillDamage);
-            }
-
-
-            for (int i = 0; i < Foods.kirbyFoods.size(); i ++) {
-                // TODO: change to read from current Kirby character
-                System.out.println("Skill description: " + Foods.kirbyFoods.get(i).foodName);
-                System.out.println("Skill damage: " + Foods.kirbyFoods.get(i).healAmount);
-            }
-             **/
+            System.out.println("Kirby has " + skillsList.size() + " powerful skill(s)");
+            System.out.println("Kirby has " + foodsList.size() + " yummy food(s) equipped");
+            System.out.println("Kirby's offense level is " + this.offense);
+            System.out.println("Kirby's offense level is " + this.defense + "\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
         }
     }
 
@@ -89,7 +114,10 @@ public class CSMidterm {
 
             System.out.println("What would you like to do?\n" +
                     "1 - Learn a new skill\n" +
-                    "2 - Equip a new food");
+                    "2 - Equip a new food\n" +
+                    "3 - Read skills\n" +
+                    "4 - Read foods equipped\n" +
+                    "5 - Read stats");
             int userInputChoice = sc.nextInt();
 
             switch (userInputChoice) {
@@ -99,20 +127,45 @@ public class CSMidterm {
 
                     System.out.println("How much damage does this skill do?:\nEnter a number: ");
                     double skillDamageInput = sc.nextDouble();
-
                     a.createSkill(skillDescriptionInput, skillDamageInput);
+                    a.readStats();
                     break;
+
                 case 2:
                     System.out.println("What's the name of your food?");
                     String foodNameInput = sc.next();
 
                     System.out.println("How much does your food heal?\nEnter a number:");
                     double healAmountInput = sc.nextInt();
-
                     a.createFood(foodNameInput, healAmountInput);
+                    a.readStats();
+                    break;
+                case 3:
+                    ArrayList<Skills> skillsList = a.readSkills();
+                    System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
 
+                    for (int i = 0; i < skillsList.size(); i ++) {
+                        int currentSkill = i + 1;
+                        System.out.println("Skill number " + currentSkill + ": ");
+                        System.out.println("    - Skill Description: " + skillsList.get(i).skillDescription);
+                        System.out.println("    - Skill Damage: " + skillsList.get(i).skillDamage + "\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+                    }
+                    break;
+                case 4:
+                    ArrayList<Foods> foodsList = a.readFoods();
+                    System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+
+                    for (int i = 0; i < foodsList.size(); i ++) {
+                        int currentFood = i + 1;
+                        System.out.println("Food number " + currentFood);
+                        System.out.println("    - Food Title: " + foodsList.get(i).foodName);
+                        System.out.println("    - Heal Amount: " + foodsList.get(i).healAmount + "\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+                    }
+                    break;
+                case 5:
+                    a.readStats();
+                    break;
             }
-            a.readStats();
         }
 
 
