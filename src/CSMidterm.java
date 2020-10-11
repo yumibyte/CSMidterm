1ef03cad9b1c7d5fdac0886aeaa41ea92e3a103a
Yumibyte <ashraigosa@gmail.com>
2020-10-10 20:58:46 -0700

    create new skill

--- src/CSMidterm.java
+++ src/CSMidterm.java
@@ -4,71 +4,83 @@ import java.util.Scanner;
 public class CSMidterm {
 
     public static class Skills {
-        public String skillDescription;
-        public int skillDamage;
-    }
+        public static ArrayList<Skills> kirbySkills = new ArrayList<Skills>();
 
-    public static class Kirby extends Skills {
+        private String skillDescription;
+        private int skillDamage;
 
-        public String username;
-        public int health;
-
-        public String type;
-        static ArrayList<Kirby> kirbyCharacters = new ArrayList<Kirby>();
-
-
-        Kirby(String username) {
-            this.health = 100;
-            this.username = username;
-//            this.skillDecription = skillDescription;
-//            this.skillDamage = skillDamage;
-            kirbyCharacters.add(this);
+        Skills(String skillDescription, int skillDamage) {
+            this.skillDescription = skillDescription;
+            this.skillDamage = skillDamage;
+            kirbySkills.add(this);
         }
 
-//        Kirby(String skillDescription, int skillDamage) {
-//            this.skillDescription = skillDescription;
-//            this.skillDamage = skillDamage;
+    }
+
+    public static class Kirby {
+
+        private String username;
+        private int health;
+
+        Kirby(String username) {
+            this.health = 100;      // Predefined health
+            this.username = username;
 //            kirbyCharacters.add(this);
-//        }
+        }
+
+        void createSkill(String skillDescription, int skillDamage) {
+            Skills skills = new Skills(skillDescription, skillDamage);
+
+        }
 
         public void readInstances() {
 
-            for (int i = 0; i < kirbyCharacters.size(); i ++) {
+            // setup to read KirbyCharacters
+            System.out.println("username: " + this.username);
+            System.out.println("health: " + this.health);
+
+            for (int i = 0; i < Skills.kirbySkills.size(); i ++) {
                 // TODO: change to read from current Kirby character
-                System.out.println("username: " + kirbyCharacters.get(i).username);
-                System.out.println("character damage: " + kirbyCharacters.get(i).skillDamage);
+                System.out.println("Skill description: " + Skills.kirbySkills.get(i).skillDescription);
+                System.out.println("Skill damage: " + Skills.kirbySkills.get(i).skillDamage);
             }
         }
     }
 
     public static void main(String[] args) {
+
+        // Begin by creating user then enter forever loop of creating skills/equipping food
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to create a Kirby!\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n" +
+                "What is your username?\n" +
+                "Enter name: ");
+        String username = sc.next();
+
+        // initialize Kirby with username
+        Kirby a = new Kirby(username);
+
         while (true) {
-            Scanner sc = new Scanner(System.in);
 
-            System.out.println("Welcome to create a Kirby!\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n" +
-                    "What is your username?\n" +
-                    "Enter name: ");
+            System.out.println("What would you like to do?\n" +
+                    "1 - Learn a new skill\n" +
+                    "2 - Equip a new food");
+            int userInputChoice = sc.nextInt();
 
-            String username = sc.next();
-            Kirby a = new Kirby(username);
-
-            System.out.println("What skill type will your Kirby have?\n" +
-                    "1 - Bomb\n" +
-                    "2 - Magical\n" +
-                    "3 - Ice\n" +
-                    "Number input 1-3: ");
-            int userInputType = sc.nextInt();
-            switch (userInputType) {
+            switch (userInputChoice) {
                 case 1:
-                    a.type = "Bomb";
+                    System.out.println("Give a short description of your new skill: ");
+                    String skillDescriptionInput = sc.next();
+
+                    System.out.println("How much damage does this skill do?:\nEnter a number: ");
+                    int skillDamageInput = sc.nextInt();
+
+                    a.createSkill(skillDescriptionInput, skillDamageInput);
                     break;
                 case 2:
-                    a.type = "Magical";
-                    break;
-                case 3:
-                    a.type = "Ice";
-                    break;
+                    System.out.println("equip food");
             }
+
+
 
             a.readInstances();
         }
